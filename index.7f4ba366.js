612e9735ed3f37527fac2acc5702db2d08d2e6ec
Iryna Yehorova <yehorova.dev@gmail.com>
2024-07-28 22:46:39 +0300

    make build

---
+++ index.7f4ba366.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var r=e[l].cells.length,o=e[l].cells[r-1],c=e[l].cells[1].cloneNode(!0);e[l].insertBefore(c,o)}
+//# sourceMappingURL=index.7f4ba366.js.map
